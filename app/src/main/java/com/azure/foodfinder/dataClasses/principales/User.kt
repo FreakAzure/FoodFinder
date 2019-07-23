cb42f350218790709c66afd8b0bdf0941539a727
FreakAzure <oberyou@gmail.com>
2019-07-23 22:42:30 +0200

    pretty much nothing new but still

---
+++ app/src/main/java/com/azure/foodfinder/dataClasses/principales/User.kt
@@ -0,0 +1,6 @@
+package com.azure.foodfinder.dataClasses.principales
+
+data class User(
+    val userName: String,
+    val UID: String
+)
--- app/src/main/java/com/azure/foodfinder/fragments/logIn/RegisterFragment.kt
+++ app/src/main/java/com/azure/foodfinder/fragments/logIn/RegisterFragment.kt
@@ -11,7 +11,9 @@ import com.azure.foodfinder.MainActivity
 import com.azure.foodfinder.Navigation.Navigator.ChangeActivity
 import com.azure.foodfinder.Navigation.Navigator.ChangeFragment
 import com.azure.foodfinder.R
+import com.azure.foodfinder.dataClasses.principales.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
@@ -29,7 +31,10 @@ class RegisterFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         TAG = "Main"
         registerBtn.setOnClickListener {
+
+
             createUser()
+
         }
 
 
@@ -44,12 +49,14 @@ class RegisterFragment : Fragment() {
             Toast.makeText(context, "Please, input email and pasword to create a user", Toast.LENGTH_SHORT).show()
         }
         val auth = FirebaseAuth.getInstance()
+
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
             if (task.isSuccessful) {
                 // Sign in success, update UI with the signed-in user's information
                 val user = FirebaseAuth.getInstance().currentUser
                 Log.d(TAG, "createUserWithEmail:success, user uid: ${user?.uid}")
                 startActivity( ChangeActivity(MainActivity(), context, false))
+                saveUserToFirebaseDataBase()
                 activity?.finish()
 
 
@@ -61,9 +68,21 @@ class RegisterFragment : Fragment() {
                     Toast.LENGTH_LONG
                 ).show()
             }
-
-            // ...
         }
+
+
+    }
+
+    fun saveUserToFirebaseDataBase(){
+
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val db = FirebaseDatabase.getInstance().reference
+        val userName = userName.text.toString()
+        val user = User(userName, uid)
+        Log.d("Main", user.toString())
+        db.child("users").child("UID").setValue(uid)
+        db.child("users").child(uid).setValue(user)
+
     }
 
 
